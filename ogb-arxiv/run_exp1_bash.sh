feb35f09eff555aaf90c21d9f828e5649efa1140
Weilin Cong <CongWeilin@users.noreply.github.com>
2022-08-08 06:19:49 -0700

    update

--- ogb-arxiv/run_exp1_bash.sh
+++ ogb-arxiv/run_exp1_bash.sh
@@ -1,13 +1,4 @@
 
-# python exp1_graph_influence.py --num_remove_nodes 0.02
-# python exp1_graph_influence.py --num_remove_nodes 0.05
-# python exp1_graph_influence.py --num_remove_nodes 0.10
-
-# python exp1_graph_fisher.py --num_remove_nodes 0.02
-# python exp1_graph_fisher.py --num_remove_nodes 0.05
-# python exp1_graph_fisher.py --num_remove_nodes 0.10
-
-# python exp1_graph_eraser.py --parallel_unlearning 1 --hop_neighbors 15
 # python exp1_graph_projector.py --parallel_unlearning 1
 
 # for SEED in 0 10 123 231 321; do
